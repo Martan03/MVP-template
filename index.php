182289f60a8fd50eec3655416ca7d6eef968dcfc
Martan03 <martinslezak03@gmail.com>
2023-01-25 21:47:44 +0100

    Edit index

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 mb_internal_encoding('UTF-8');
 
+/**
+ * Autoloads files so you don't have to require every file manually
+ */
 spl_autoload_register(function (string $class) {
     if (str_ends_with($class, "Presenter"))
         require("presenter/" . $class . ".php");
